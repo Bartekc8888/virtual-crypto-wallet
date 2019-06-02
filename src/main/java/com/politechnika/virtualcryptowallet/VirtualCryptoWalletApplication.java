354d66f4cfa6e5a2f54aca818fc40cd224ab1195
Bartłomiej Ciesielski <bartekc8@op.pl>
2019-06-02 20:09:44 +0200

    Initialize repository.

---
+++ src/main/java/com/politechnika/virtualcryptowallet/VirtualCryptoWalletApplication.java
@@ -0,0 +1,13 @@
+package com.politechnika.virtualcryptowallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualCryptoWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualCryptoWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/politechnika/virtualcryptowallet/VirtualCryptoWalletApplicationTests.java
@@ -0,0 +1,16 @@
+package com.politechnika.virtualcryptowallet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VirtualCryptoWalletApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
