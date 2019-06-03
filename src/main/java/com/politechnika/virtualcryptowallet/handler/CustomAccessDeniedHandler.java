b4cf529fea0144e5372fee793fbbd4a9b5525394
Bartłomiej Ciesielski <bartekc8@op.pl>
2019-06-03 21:55:38 +0200

    Add logger.

--- src/main/java/com/politechnika/virtualcryptowallet/handler/CustomAccessDeniedHandler.java
+++ src/main/java/com/politechnika/virtualcryptowallet/handler/CustomAccessDeniedHandler.java
@@ -1,27 +1,25 @@
 package com.politechnika.virtualcryptowallet.handler;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
+@Slf4j
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
-    public void handle(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            AccessDeniedException exc) throws IOException, ServletException {
-
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException exc) throws IOException {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        log.error("Access denied!");
+
         if (auth != null) {
-            System.out.println(auth.getDetails());
+            log.error("Auth details: ", auth.getDetails());
         }
 
         response.sendRedirect(request.getContextPath() + "/accessDenied");
